f5f0c8d2e5d768564d70dfe8b45479c8bed3df24
adhfmz7 <ahmadaldasouqi559@gmail.com>
2022-08-01 20:47:25 +0300

    commit

---
+++ ndarray.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+arr = np.array([[3, 2, 1],
+                [6, 5, 4]])
+print(type(arr.shape))
+
+class ndarray:
+    
+    def __init__(shape, dtype=float, ):
+      pass 
+       
+def array(object, dtype=None):
+    pass
