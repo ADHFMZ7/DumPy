7ce1e3a0b59d603373b83a6047bf91c54f1affbf
adhfmz7 <ahmadaldasouqi559@gmail.com>
2023-03-17 20:40:39 -0700

    document header file

--- src/matrix.h
+++ src/matrix.h
@@ -1,17 +1,21 @@
 #ifndef MATRIX
 #define MATRIX
 
+
+
 typedef struct {
     float* entries;
     int N;
 } Matrix ;
 
+// Matrix Constructors
 Matrix* create(const int N);
 Matrix* randmat(const int N);
 Matrix* eye(const int N);
 Matrix* load(float* new, const int N);
 
 
+// Math operation
 float* transpose(float* A, int n, int N);
 float* naive_transpose(float* A, int N);
 Matrix* matmul(Matrix* A, Matrix* B);
@@ -20,6 +24,7 @@ Matrix* floatmul(Matrix* A, float B);
 Matrix* matadd(Matrix* A, Matrix* B);
 Matrix* floatadd(Matrix* A, float B);
 
+// getters/setters
 int getn(Matrix* A);
 float at(Matrix* A, int i, int j);
 void set(Matrix* A, int i, int j, float val);
